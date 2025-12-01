db93f91c1d49c40c8f60381dfbf040a3e43b3dfb
TasconLD <lauradayanatascon@hotmail.com>
2025-11-30 22:57:04 -0500

    tablero vacio

--- src/main/java/com/batallanaval/batallanaval/BatallaNavalApp.java
+++ src/main/java/com/batallanaval/batallanaval/BatallaNavalApp.java
@@ -3,21 +3,16 @@ package com.batallanaval.batallanaval;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class BatallaNavalApp extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
-        // La ruta empieza desde resources
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/batallanaval/view/JuegoView.fxml"));
-        AnchorPane root = loader.load();
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/com/batallanaval/batallanaval/view/JuegoView.fxml")
+        );
 
-        Scene scene = new Scene(root, 600, 700);
-        scene.getStylesheets().add(getClass().getResource("/com/batallanaval/view/estilos.css").toExternalForm());
-
-        stage.setTitle("Batalla Naval");
+        Scene scene = new Scene(loader.load());
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.batallanaval.batallanaval {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens com.batallanaval.controller to javafx.fxml;
 
-    opens com.batallanaval.batallanaval to javafx.fxml;
     exports com.batallanaval.batallanaval;
 }
